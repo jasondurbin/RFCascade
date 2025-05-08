00d5c95c918fc5901efeb6c552ab803506ccdd14
Jason Durbin <jason.durbin@neonphysics.com>
2025-05-07 19:14:24 -0700

    Added handling for hex only color.

--- js/cmap/cmap-util.js
+++ js/cmap/cmap-util.js
@@ -19,23 +19,31 @@ export class ColormapControl{
 		});
 	}
 	addEventListener(e, callback){ this.selector.addEventListener(e, callback); }
-	cmap(){
+	/**
+	 * Create and return cmap finder.
+	 *
+	 * @param {Boolean} [hex] Should HEX (#000) be enforced instead of rgb()?
+	 *
+	 * @returns {(v: Number) => String} Function to convert value to color
+	 */
+	cmap(hex){
 		const cms = this.constructor.Colormaps;
 		const find_colormap = this.constructor.find_colormap;
 		for (let i = 0; i < cms.length; i++)
 			if (this.selector[i].selected)
-				return find_colormap(cms[i]);
-		return find_colormap(this.defaultSelection);
+				return find_colormap(cms[i], hex);
+		return find_colormap(this.defaultSelection, hex);
 	}
 }
 
 /**
  * Convert HSV to RGB.
- * @param {Number} h - Hue (0-1).
- * @param {Number} s - Saturation (0-1).
- * @param {Number} v - Value (0-1).
  *
- * @returns {String} - rbg color.
+ * @param {Number} h Hue (0-1).
+ * @param {Number} s Saturation (0-1).
+ * @param {Number} v Value (0-1).
+ *
+ * @returns {[Number, Number, Number]} rbg color (0-1).
  */
 export function hsv2rgb(h, s, v){
 	let r, g, b;
@@ -78,6 +86,35 @@ export function hsv2rgb(h, s, v){
 			b = q;
 			break;
 	}
-
-	return `rgb(${Math.round(r * 255)},${Math.round(g * 255)},${Math.round(b * 255)})`;
+	return [r, g, b];
 }
+
+const _hex = (c) => {
+	const v = Math.min(255, Math.max(0, Math.round(c*255))).toString(16);
+	return v.length == 1 ? "0" + v : v
+}
+
+/**
+ * Convert RGB to Hex.
+ *
+ * @param {Number} r Red (0-1).
+ * @param {Number} g Green (0-1).
+ * @param {Number} b Blue (0-1).
+ *
+ * @returns {String} hex color.
+ */
+export function rgb2hex(r, g, b){return `#${_hex(r)}${_hex(g)}${_hex(b)}`;}
+
+const _c = (v) => {
+	return Math.min(255, Math.max(0, v*255));
+}
+/**
+ * Convert RGB to RGB string.
+ *
+ * @param {Number} r Red (0-1).
+ * @param {Number} g Green (0-1).
+ * @param {Number} b Blue (0-1).
+ *
+ * @returns {String} rgb() string.
+ */
+export function rgb2rgb(r, g, b){return `rgb(${_c(r)},${_c(g)},${_c(b)})`;}
