45a918eae1439c2cf1a0018c6cb1193ac13f8ef6
Jason Durbin <jason.durbin@neonphysics.com>
2025-05-07 19:14:06 -0700

    Changed to automatically generated (separate) file.

--- js/cmap/cmap-listed.js
+++ js/cmap/cmap-listed.js
@@ -1,88 +1,45 @@
-import {ColormapControl} from "./cmap-util.js"
+import {ColormapControl, rgb2hex, rgb2rgb} from "./cmap-util.js"
+import {ListedColormaps} from "./cmap-listed-colors.js"
+
+
+const cms = [];
+ListedColormaps.forEach((c) => {
+	cms.push(c.name);
+})
+ListedColormaps.forEach((c) => {
+	if (c.reversable) cms.push(c.name + "_r");
+})
 
 export class ListedColormapControl extends ColormapControl{
-	static Colormaps = [
-		'Vibrant',
-		'Bright',
-		'Muted',
-		'Vibrant_r',
-		'Bright_r',
-		'Muted_r',
-	]
-	static find_colormap = find_colormap;
+	static Colormaps = cms
+	static find_colormap = find_listed_colormap;
 }
-/**
- * @callback Colormapper
- * @param {number} val - Value for colormap. Will be bound [0, 1].
- *
- * @return {string} Color as `rgb()` string.
- */
 
 /**
  * Find and return colormap based on input `name`.
  *
- * @param {string} name
+ * @param {String} name
+ * @param {Boolean} hex Should hex (#000000) be enforced instead of "rgb()"?
  *
- * @return {Colormapper}
+ * @return {(v: Number) => String} Function to convert input value to color string.
  * */
-export function find_colormap(name){
+export function find_listed_colormap(name, hex){
 	const rev = name.endsWith('_r');
-	switch (name){
-		case 'Bright':
-		case 'Bright_r':
-			return _array_wrapper(_tol_bright, rev);
-
-		case 'Vibrant':
-		case 'Vibrant_r':
-			return _array_wrapper(_tol_vibrant, rev);
-
-		case 'Muted':
-		case 'Muted_r':
-			return _array_wrapper(_tol_muted, rev);
-
-		default:
-			throw Error(`Missing colormap: ${name}.`)
-	}
+	if (rev) name = name.substring(0, name.length - 2);
+	for (let i = 0; i < ListedColormaps.length; i++) if (ListedColormaps[i].name == name) return _array_wrapper(ListedColormaps[i].colors, rev, hex);
+	throw Error(`Missing colormap: ${name}.`)
 }
 
-function _array_wrapper(colors, reverse){
+function _array_wrapper(colors, reverse, hex){
 	if (reverse === undefined) reverse = false;
+	if (hex === undefined) hex = false;
+
 	return (value) => {
-		if (isNaN(value)) return `rgb(0,0,0)`
-		if (!isFinite(value)) return `rgb(0,0,0)`
+		if (isNaN(value)) return '#000'
+		if (!isFinite(value)) return '#000'
 		if (reverse) value = colors.length - value - 1;
-		return colors[value % colors.length];
+		const [r, g, b] = colors[value % colors.length]
+		if (!hex) return rgb2rgb(r, g, b);
+		return rgb2hex(r, g, b);
 	}
 }
-
-const _tol_bright = [
-	"#4477AA",
-	"#EE6677",
-	"#228833",
-	"#CCBB44",
-	"#66CCEE",
-	"#AA3377",
-	"#BBBBBB",
-]
-
-const _tol_vibrant = [
-	"#EE7733",
-	"#0077BB",
-	"#33BBEE",
-	"#EE3377",
-	"#CC3311",
-	"#009988",
-	"#BBBBBB",
-]
-
-const _tol_muted = [
-	"#CC6677",
-	"#332288",
-	"#DDCC77",
-	"#117733",
-	"#88CCEE",
-	"#882255",
-	"#44AA99",
-	"#999933",
-	"#AA4499",
-]
